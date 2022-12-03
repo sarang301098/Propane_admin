81cf2f4b69e8becc8fd6885c0771abf51dd3c30b
sarang301098 <sarangp3010@gmail.com>
2022-12-02 20:00:42 -0500

    version change

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -4,7 +4,7 @@ import { Middleware, MiddlewareAPI, Dispatch, AnyAction, Action } from "redux";
 import { loginSuccess, logout } from "../store/auth/auth.action";
 
 export const API = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "http://127.0.0.1:3333",
 });
 
 export const apiMiddleware: Middleware = (storeApi: MiddlewareAPI<any>) => (
